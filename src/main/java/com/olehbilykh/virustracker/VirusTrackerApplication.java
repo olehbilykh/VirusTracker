8ba62c96e6b2170ba54c331aaa7d4c7feb0326d0
olehbilykh <olehbilykh@gmail.com>
2021-09-21 21:57:49 +0300

    initial

---
+++ src/main/java/com/olehbilykh/virustracker/VirusTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.olehbilykh.virustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirusTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirusTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/olehbilykh/virustracker/VirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olehbilykh.virustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirusTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
